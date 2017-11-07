35be229b42702d544ea52f6a69f606509cefdfd2
kristiinasim <33310254+kristiinasim@users.noreply.github.com>
2017-11-07 09:17:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+namespace Grupitöö
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"C:\Users\opilane\Desktop\TEXTS");
+            FileInfo[] Files = d.GetFiles("*.txt"); 
+            string str = "";
+            foreach (FileInfo file in Files)
+            {
+                string i = str + " " + file.Name;
+                Console.WriteLine("Failid on" + i);
+                Console.ReadLine();
+            }
+
+            
+        }
+    }
+}
