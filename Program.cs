6ec69d1d176ca938fdff3aa0aeaaa5f908aa3a22
kristiinasim <33310254+kristiinasim@users.noreply.github.com>
2017-11-07 09:32:33 +0200

    Uus

--- Program.cs
+++ Program.cs
@@ -9,14 +9,17 @@ namespace Grupitöö
             DirectoryInfo d = new DirectoryInfo(@"C:\Users\opilane\Desktop\TEXTS");
             FileInfo[] Files = d.GetFiles("*.txt"); 
             string str = "";
+            Console.WriteLine("Olemasolevad failid on:");
             foreach (FileInfo file in Files)
             {
-                string i = str + " " + file.Name;
-                Console.WriteLine("Failid on" + i);
-                Console.ReadLine();
-            }
+                string i = str + "" + file.Name;
+                Console.WriteLine(i);
 
+            }
             
+            Console.ReadLine();
+
+
         }
     }
 }
