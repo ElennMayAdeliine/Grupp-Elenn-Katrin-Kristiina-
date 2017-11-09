efe48ed9bd4a9da6def3a2d1c8ffb44152fad99e
ElennMayAdeliine <33444865+ElennMayAdeliine@users.noreply.github.com>
2017-11-09 09:46:38 +0200

    salvestamine

---
+++ 8.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kas soovite salvestada või mitte? Kui jah siis kirjutage Save ja kui mitte siis Canel.");
+            string vastus = "";
+
+            if (vastus == "Save")
+            {
+                Failinimi.ShowDialog();
+            }
+            
+        }
+    }
+}
