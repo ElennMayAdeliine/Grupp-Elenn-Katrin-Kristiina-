a5d69b9869c70ea0cdc04b6d5f96f826891617c6
triinutamm <triinu.tamm.1995@gmai.com>
2017-11-14 09:49:50 +0200

    asukoha string, ja midagi

--- Program.cs
+++ Program.cs
@@ -7,9 +7,10 @@ namespace Grupitöö
         //https://www.codeguru.com/csharp/csharp/cs_syntax/anandctutorials/article.php/c5861/Working-with-Files-in-C.htm - SIIN ON LINK ERINEVATE VAJALIKE TERMINITE JMS-GA
         static void Main(string[] args)
         {
+            string asukoht = @"C:\Users\Kasutaja\Desktop\kool\programmeerimine\rühmatöö\";
             Start:
-            DirectoryInfo d = new DirectoryInfo(@"C:\Users\opilane\Desktop\TEXTS");
-            FileInfo[] Files = d.GetFiles("*.txt"); 
+            DirectoryInfo d = new DirectoryInfo(asukoht);
+            FileInfo[] Files = d.GetFiles("*.txt");
             string str = "";
             Console.WriteLine("Olemasolevad failid on:");
             foreach (FileInfo file in Files)
@@ -18,14 +19,14 @@ namespace Grupitöö
                 Console.WriteLine(i);
 
             }
-            
-            Console.ReadLine();
+
+            Console.WriteLine(" ");
 
             //New file
             Console.WriteLine("Mida soovite teha?");
-            Console.WriteLine("Loe faili - Read");
-            Console.WriteLine("Loo uus fail - New ");
-            Console.WriteLine("Kustuta fail- Delete ");
+            Console.WriteLine("Loe faili - Read FAILINIMI");
+            Console.WriteLine("Loo uus fail - New FAILINIMI");
+            Console.WriteLine("Kustuta fail- Delete FAILINIMI");
             Console.WriteLine("Lahku - Exit");
 
 
@@ -34,7 +35,7 @@ namespace Grupitöö
             {
                 Console.WriteLine("Millist faili soovite lugeda?");
                 var FAILINIMI = Console.ReadLine();
-                string text = System.IO.File.ReadAllText(@"C:\Users\opilane\Desktop\TEXTS\" + FAILINIMI + ".txt");
+                string text = System.IO.File.ReadAllText(asukoht + FAILINIMI + ".txt");
                 Console.WriteLine(text);
                 Console.ReadLine();
             }
@@ -42,32 +43,32 @@ namespace Grupitöö
             {
                 Console.WriteLine("Millist faili soovite kustutada?");
                 var FAIL = Console.ReadLine();
-                System.IO.File.Delete(@"C:\Users\opilane\Desktop\TEXTS\" + FAIL + ".txt");
+                System.IO.File.Delete(asukoht + FAIL + ".txt");
                 Console.WriteLine("Aitäh, fail on kustutatud!");
                 Console.ReadLine();
 
             }
-        
+
             if (val == "New")
             {
                 Console.WriteLine("Sisestage uue faili nimi:");
                 string fileName = Console.ReadLine();
-                File.Create(@"C:\Users\opilane\Desktop\TEXTS" + fileName + ".txt");
+                File.Create(asukoht + fileName + ".txt");
                 Console.ReadLine();
             }
-            {
-            
-            if (val == "Exit")
             {
 
-            }
-            else
-            {
-                Console.WriteLine("Valisite vale variandi! Olemasolevad variandid on: Read, New, Delete, Exit. Vajutage enterit ja proovige uuesti!");
-                Console.ReadLine();
-                goto Start;
+                if (val == "Exit")
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("Valisite vale variandi! Olemasolevad variandid on: Read, New, Delete, Exit. Vajutage enterit ja proovige uuesti!");
+                    Console.ReadLine();
+                    goto Start;
+                }
             }
         }
     }
-  }
 }
